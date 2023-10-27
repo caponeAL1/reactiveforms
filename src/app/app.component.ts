6d2c1abff5c3033794ba192e230196a050008df8
aravind.ln <aravind.ln@gavstech.com>
2023-10-27 21:29:32 +0530

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +6,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'reactiveforms';
+  userList: any = [];
+  http: any;
+  addUser(udata:any)
+  {
+    // console.log(udata);
+    // console.log(udata.value);
+    // this.userList.push(udata.value);
+    this.http.post('http://localhost:9901/insert',udata.value).
+    subscribe((response: any)=>
+    {
+      console.log(response);
+    })
+  }
 }
